3f5f83967697c920b40e043c6a0a464d83a612d0
vinhluc1984 <94994150+vinhluc1984@users.noreply.github.com>
2026-04-01 16:51:55 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+﻿const CACHE_NAME = 'vnlearner-v1';
+const ASSETS = [
+  'standalone_game.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
