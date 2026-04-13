41d34669ba671ff27c8d45877bcdc891939e8514
vinhluc1984 <94994150+vinhluc1984@users.noreply.github.com>
2026-04-13 13:55:15 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vnlearner-v2';
+const CACHE_NAME = 'vnlearner-v4';
 const ASSETS = [
   './',
   'index.html',
@@ -8,6 +8,17 @@ const ASSETS = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
   );
 });
 
